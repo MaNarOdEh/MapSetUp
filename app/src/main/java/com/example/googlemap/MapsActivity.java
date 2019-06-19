f46bcadf0108e826482668feca6d89d9b3dc85e0
MaNarOdEh <manar-_odeh@hotmail.com>
2019-06-19 21:59:41 +0300

    New Editid

--- app/src/main/java/com/example/googlemap/MapsActivity.java
+++ app/src/main/java/com/example/googlemap/MapsActivity.java
@@ -7,8 +7,12 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -17,10 +21,21 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity
         implements OnMapReadyCallback {
@@ -30,12 +45,21 @@ public class MapsActivity extends FragmentActivity
     private  static final String LOCATION_PREMISSION=Manifest.permission.ACCESS_FINE_LOCATION;
     private static final int MY_PREMISSION=333;
     private FusedLocationProviderClient fusedLocationProviderClient;
+    private TextView txt_gbs,txt_sessons,text_temp;
+    String OPEN_WEATHER_MAP_API = "d32d64021d1cb92523a372dd36a23037";
+    MarkerOptions options;
+    Marker marker;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        txt_sessons=findViewById(R.id.txt_sessons);
+        txt_gbs=findViewById(R.id.txt_gbs);
+        text_temp=findViewById(R.id.text_temp);
+        txt_sessons=findViewById(R.id.txt_sessons);
         accessLocation();
 
     }
@@ -157,12 +181,103 @@ public class MapsActivity extends FragmentActivity
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(32, 35);
+         options = new MarkerOptions()
+                .position(sydney)
+                .title("I am here!");
+       // marker= mMap.addMarker(new MarkerOptions().position(sydney).title("Palestine"));
+        Geocoder geo = new Geocoder(this.getApplicationContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = geo.getFromLocation(32, 35, 1);
+            if (addresses.isEmpty()) {
+            //    addres.setText("Waiting for Location");
+            } else {
+                if (addresses.size() > 0) {
+                    marker= mMap.addMarker(new MarkerOptions().position(sydney).title(addresses.get(0).getAddressLine(0)));
+                    options.title(addresses.get(0).getAddressLine(0));
+                    //txt_sessons.setT
+                    //addres.setText(addresses.get(0).getFeatureName() + ", " + addresses.get(0).getLocality() + ", " + addresses.get(0).getAdminArea() + ", " + addresses.get(0).getCountryName());
+                    //Toast.makeText(getApplicationContext(), "Address:- " + addresses.get(0).getFeatureName() + addresses.get(0).getAdminArea() + addresses.get(0).getLocality(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }catch(Exception e){
+
+        }
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        //googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        //googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        //googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                if(marker!=null){
+                    marker.remove();
+                }
+                LatLng lat = new LatLng(latLng.latitude, latLng.longitude);
+                 options = new MarkerOptions()
+                        .position(lat)
+                        .title("I am here!");
+                Geocoder geo = new Geocoder(MapsActivity.this, Locale.getDefault());
+                try {
+                    List<Address> addresses = geo.getFromLocation(32, 35, 1);
+                    if (addresses.isEmpty()) {
+                        //    addres.setText("Waiting for Location");
+                    } else {
+                        if (addresses.size() > 0) {
+                            marker= mMap.addMarker(new MarkerOptions().position(lat).title(addresses.get(0).getAddressLine(0)));
+                            options.title(addresses.get(0).getAddressLine(0));
+                            //addres.setText(addresses.get(0).getFeatureName() + ", " + addresses.get(0).getLocality() + ", " + addresses.get(0).getAdminArea() + ", " + addresses.get(0).getCountryName());
+                            //Toast.makeText(getApplicationContext(), "Address:- " + addresses.get(0).getFeatureName() + addresses.get(0).getAdminArea() + addresses.get(0).getLocality(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }catch(Exception e){
+                }
+                marker=mMap.addMarker(options);
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                txt_gbs.setText( Math.round(latLng.latitude)+","+Math.round(latLng.longitude)+"  ");
+            }
+        });
         googleMap.getUiSettings().setZoomGesturesEnabled(true);
         // and move the map's camera to the same location.
+    }
 
+    public static String excuteGet(String targetURL)
+    {
+        URL url;
+        HttpURLConnection connection = null;
+        try {
+            //Create connection
+            url = new URL(targetURL);
+            connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestProperty("content-type", "application/json;  charset=utf-8");
+            connection.setRequestProperty("Content-Language", "en-US");
+            connection.setUseCaches (false);
+            connection.setDoInput(true);
+            connection.setDoOutput(false);
 
+            InputStream is;
+            int status = connection.getResponseCode();
+            if (status != HttpURLConnection.HTTP_OK)
+                is = connection.getErrorStream();
+            else
+                is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            return null;
+        } finally {
+            if(connection != null) {
+                connection.disconnect();
+            }
+        }
     }
 }
